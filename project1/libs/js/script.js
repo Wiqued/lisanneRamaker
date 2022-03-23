e7063e44c99b7306d81784afef90e87ba31d5970
Lisanne R <lisanneramaker@hotmail.com>
2022-03-23 12:40:23 +0000

    Set up leaflet

---
+++ project1/libs/js/script.js
@@ -0,0 +1,41 @@
+/* $('#btnRun').click(function() {
+
+    $.ajax({
+        url: "libs/php/getCountryInfo.php",
+        type: 'POST',
+        dataType: 'json',
+        data: {
+            country: $('#selCountry').val(),
+            lang: $('#selLanguage').val()
+        },
+        success: function(result) {
+
+            console.log(JSON.stringify(result));
+
+            if (result.status.name == "ok") {
+
+                $('#txtContinent').html(result['data'][0]['continent']);
+                $('#txtCapital').html(result['data'][0]['capital']);
+                $('#txtLanguages').html(result['data'][0]['languages']);
+                $('#txtPopulation').html(result['data'][0]['population']);
+                $('#txtArea').html(result['data'][0]['areaInSqKm']);
+
+            }
+        
+        },
+        error: function(jqXHR, textStatus, errorThrown) {
+            // your error code
+        }
+    }); 
+
+}); */
+
+var map = L.map('map').setView([51.505, -0.09], 13);
+
+var Thunderforest_Neighbourhood = L.tileLayer('https://{s}.tile.thunderforest.com/neighbourhood/{z}/{x}/{y}.png?apikey=e24c409ff68c47bb974a643883a6842b', {
+    attribution: '&copy; <a href="http://www.thunderforest.com/">Thunderforest</a>, &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+    apikey: 'e24c409ff68c47bb974a643883a6842b',
+    maxZoom: 22
+});
+
+L.tileLayer.provider('Thunderforest.Landscape', {apikey: 'e24c409ff68c47bb974a643883a6842b'}).addTo(map);
