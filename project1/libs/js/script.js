469731f5e8e0c5555eccb492531f138f21fdd321
Lisanne R <lisanneramaker@hotmail.com>
2022-03-25 15:03:42 +0000

    On click gives country name

--- project1/libs/js/script.js
+++ project1/libs/js/script.js
@@ -1,74 +1,62 @@
-/* $('#btnRun').click(function() {
+// Get Country name based on Lat/Lng
+function getCountryName(lat, lng) {
 
     $.ajax({
-        url: "libs/php/getCountryInfo.php",
+        url: "libs/php/getCountry.php",
         type: 'POST',
         dataType: 'json',
         data: {
-            country: $('#selCountry').val(),
-            lang: $('#selLanguage').val()
+            lat: lat,
+            lng: lng
         },
-        success: function(result) {
+        success: function (result) {
 
             console.log(JSON.stringify(result));
 
             if (result.status.name == "ok") {
 
-                $('#txtContinent').html(result['data'][0]['continent']);
-                $('#txtCapital').html(result['data'][0]['capital']);
-                $('#txtLanguages').html(result['data'][0]['languages']);
-                $('#txtPopulation').html(result['data'][0]['population']);
-                $('#txtArea').html(result['data'][0]['areaInSqKm']);
+                if ("countryName" in result.data) {
 
+                    console.log(result['data']['countryName']);
+
+                }
             }
-        
         },
-        error: function(jqXHR, textStatus, errorThrown) {
-            // your error code
+        error: function (jqXHR, textStatus, errorThrown) {
+            console.log("Find the country isn't working")
+            console.log(textStatus)
+            console.log(errorThrown)
         }
-    }); 
+    });
 
-}); */
+};
 
+// Initialise map and current location on load in
 var map = L.map('map');
 
 if (navigator.geolocation) {
-  navigator.geolocation.getCurrentPosition(showPosition);
+    navigator.geolocation.getCurrentPosition(showPosition);
 } else {
-  document.getElementById("demo").innerHTML =
-  "Geolocation is not supported by this browser.";
+    document.getElementById("demo").innerHTML =
+        "Geolocation is not supported by this browser.";
 }
 
 function showPosition(position) {
     map.setView([position.coords.latitude, position.coords.longitude], 6);
-} 
+}
 
-
-
+// Map design
 var Thunderforest_Neighbourhood = L.tileLayer('https://{s}.tile.thunderforest.com/neighbourhood/{z}/{x}/{y}.png?apikey=e24c409ff68c47bb974a643883a6842b', {
     attribution: '&copy; <a href="http://www.thunderforest.com/">Thunderforest</a>, &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
     apikey: 'e24c409ff68c47bb974a643883a6842b',
     maxZoom: 22
 });
 
-L.tileLayer.provider('Thunderforest.Landscape', {apikey: 'e24c409ff68c47bb974a643883a6842b'}).addTo(map);
+L.tileLayer.provider('Thunderforest.Landscape', { apikey: 'e24c409ff68c47bb974a643883a6842b' }).addTo(map);
 
-var geojsonFeature = {
-    "type": "Feature",
-    "properties": {
-        "name": "Coors Field",
-        "amenity": "Baseball Stadium",
-        "popupContent": "This is where the Rockies play!"
-    },
-    "geometry": {
-        "type": "Point",
-        "coordinates": [-104.99404, 39.75621]
-    }
-};
-
-L.geoJSON(geojsonFeature).addTo(map);
-
+// Get Lat/Lng on click
 function onMapClick(e) {
+    getCountryName(e.latlng.lat, e.latlng.lng);
     console.log(e.latlng);
 }
 
--- project1/libs/php/getCountry.php
+++ project1/libs/php/getCountry.php
@@ -5,7 +5,7 @@
 
 	$executionStartTime = microtime(true);
 
-	$url='http://api.geonames.org/countryInfoJSON?formatted=true&lang=' . $_REQUEST['lang'] . '&country=' . $_REQUEST['country'] . '&username=flightltd&style=full';
+	$url='http://api.geonames.org/countryCodeJSON?' . 'lat=' . $_REQUEST['lat'] . '&lng=' . $_REQUEST['lng'] . '&username=lisanner';
 
 	$ch = curl_init();
 	curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
@@ -22,7 +22,7 @@
 	$output['status']['name'] = "ok";
 	$output['status']['description'] = "success";
 	$output['status']['returnedIn'] = intval((microtime(true) - $executionStartTime) * 1000) . " ms";
-	$output['data'] = $decode['geonames'];
+	$output['data'] = $decode;
 	
 	header('Content-Type: application/json; charset=UTF-8');
 
