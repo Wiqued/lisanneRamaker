2ee1a4fd7bb68c39ef0539aa56c2747f48d93291
Lisanne R <lisanneramaker@hotmail.com>
2022-05-30 16:50:54 +0100

    Shows list of all employees

--- project2/libs/js/script.js
+++ project2/libs/js/script.js
@@ -1,13 +1,12 @@
+// On submit does not refresh page
+$("#searchForm").submit(function (e) {
+    e.preventDefault();
+});
+
+// Hides and shows the right pages
 function showAll() {
     document.getElementById("listOfAll").style.display = "block";
     document.getElementById("employeeProfile").style.display = "none";
-    document.getElementById("departments").style.display = "none";
-    document.getElementById("locations").style.display = "none";
-}
-
-function showEmployeeProfile() {
-    document.getElementById("listOfAll").style.display = "none";
-    document.getElementById("employeeProfile").style.display = "block";
     document.getElementById("departments").style.display = "none";
     document.getElementById("locations").style.display = "none";
 }
@@ -27,7 +26,7 @@ function showLocations() {
     document.getElementById("locations").style.display = "block";
 }
 
-$("#listOfAll").click(function() {
+$("#listProfile").click(function() {
     showEmployeeProfile();
 });
 
@@ -42,3 +41,62 @@ $("#navDepartments").click(function() {
 $("#navLocations").click(function() {
     showLocations();
 });
+
+getAllEmployees();
+
+// Get list of all employees, name, department and location on load
+function getAllEmployees() {
+    $.ajax({
+        url: 'libs/php/getAll.php',
+        type: 'GET',
+        dataType: 'json',
+        data: {},
+        success: function (result) {
+
+            const employees = result.data
+
+            for (const employee of employees) {
+
+                const div = document.getElementById('listProfile');
+                const clonedDiv = div.cloneNode(true);
+                document.getElementById('listOfAll').appendChild(clonedDiv);
+
+                clonedDiv.getElementsByClassName('listName')[0].innerText = `${employee.firstName} ${employee.lastName}`;
+                clonedDiv.getElementsByClassName('listDepartment')[0].innerText = employee.department;
+                clonedDiv.getElementsByClassName('listLocation')[0].innerText = employee.location;
+
+                $(clonedDiv).click(function() {
+                    showEmployeeProfile(employee.id)
+                })
+            }
+        }
+    })
+}
+
+
+function showEmployeeProfile(employeeId) {
+    $.ajax({
+        url: 'libs/php/getPersonnelByID.php',
+        type: 'GET',
+        dataType: 'json',
+        data: {
+            id: employeeId,
+        },
+        success: function (result) {
+
+            document.getElementById("employeeName").innerText = `${result.data.personnel[0].firstName} ${result.data.personnel[0].lastName}`;
+            document.getElementById("employeeDepartment").innerText = result.data.personnel[0].department;
+            document.getElementById("employeeLocation").innerText = result.data.personnel[0].department;
+            document.getElementById("employeeEmail").innerText = result.data.personnel[0].email;
+
+
+
+
+            
+            document.getElementById("listOfAll").style.display = "none";
+            document.getElementById("employeeProfile").style.display = "block";
+            document.getElementById("departments").style.display = "none";
+            document.getElementById("locations").style.display = "none";
+        }
+    })
+}
--- project2/libs/php/getAll.php
+++ project2/libs/php/getAll.php
@@ -1,7 +1,7 @@
 <?php
 
 	// example use from browser
-	// http://localhost/companydirectory/libs/php/getAll.php
+	// http://localhost/project2/libs/php/getAll.php
 
 	// remove next two lines for production
 	
@@ -34,7 +34,7 @@
 
 	// SQL does not accept parameters and so is not prepared
 
-	$query = 'SELECT p.lastName, p.firstName, p.jobTitle, p.email, d.name as department, l.name as location FROM personnel p LEFT JOIN department d ON (d.id = p.departmentID) LEFT JOIN location l ON (l.id = d.locationID) ORDER BY p.lastName, p.firstName, d.name, l.name';
+	$query = 'SELECT p.id, p.lastName, p.firstName, p.jobTitle, p.email, d.name as department, l.name as location FROM personnel p LEFT JOIN department d ON (d.id = p.departmentID) LEFT JOIN location l ON (l.id = d.locationID) ORDER BY p.lastName, p.firstName, d.name, l.name';
 
 	$result = $conn->query($query);
 	
